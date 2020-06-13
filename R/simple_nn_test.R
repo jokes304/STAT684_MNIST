367a7f2a355f0891a8225335368aee5aa489391b
Andrew Somers <aksomers@gmail.com>
2020-06-13 19:03:36 -0400

    Create simple neural net, takes long time to run, may need to random sample to ensure it completes correctly

--- R/simple_nn_test.R
+++ R/simple_nn_test.R
@@ -1,9 +1,33 @@
+
+if (! require('dslabs')) {
+  install.packages('dslabs')
+  require('dslabs')
+}
+if (! require('lattice')) {
+  install.packages('lattice')
+  require('lattice')
+}
+if (! require('neuralnet')) {
+  install.packages('neuralnet')
+  require('neuralnet')
+}
+
 mnist = read_mnist()
 
+x_train <- mnist$train$images
+y_train <- mnist$train$labels
 
-x_train = mnist$train$images
-y_train = mnist$train$labels
+x_test <- mnist$test$images
+y_test <- mnist$test$labels
 
-x_test = mnist$test$images
-y_test = mnist$test$labels
+
+x_train_df <- as.data.frame(x_train)
+
+train = cbind(y_train, x_train_df)
+names = names(x_train_df)
+
+formula <- as.formula(paste("y_train ~", paste(names[!names %in% "train"], collapse = " + ")))
+
+nn <- neuralnet(formula, train, hidden=c(5,3), linear.output=T)
+
 
