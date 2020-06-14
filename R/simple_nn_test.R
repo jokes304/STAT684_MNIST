55caab19601049db544e55533c2256171f0944e8
Andrew Somers <aksomers@gmail.com>
2020-06-14 19:03:52 -0400

    Random sampled so the neural net finishes

--- R/simple_nn_test.R
+++ R/simple_nn_test.R
@@ -1,4 +1,6 @@
 
+set.seed = 1
+
 if (! require('dslabs')) {
   install.packages('dslabs')
   require('dslabs')
@@ -23,11 +25,28 @@ y_test <- mnist$test$labels
 
 x_train_df <- as.data.frame(x_train)
 
+x_test_df <- as.data.frame(x_test)
+
 train = cbind(y_train, x_train_df)
+test = cbind(y_test,x_test_df)
+
+
+samp = sample(1:60000,size=1000,replace=FALSE)
+train_samp= train[samp,]
+
 names = names(x_train_df)
 
 formula <- as.formula(paste("y_train ~", paste(names[!names %in% "train"], collapse = " + ")))
 
-nn <- neuralnet(formula, train, hidden=c(5,3), linear.output=T)
+nn <- neuralnet(formula, train_samp, hidden=c(5,3), linear.output=T)
 
+plot(nn)
+
+pr.nn <- compute(nn, test)
+
+
+pr.nn_ <- pr.nn$net.result*(max(train$y_train)-min(train$y_train))+min(train$y_train)
+test.r <- (test$y_test)*(max(test$y_test)-min(test$y_test))+min(test$y_test)
+
+MSE.nn <- sum((test.r - pr.nn_)^2)/nrow(test)
 
